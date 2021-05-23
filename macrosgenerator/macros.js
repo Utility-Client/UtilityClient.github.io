862ce318d2343f57e9b0d02d982592ee4f16371a
GamingCrafthd <kvvmischl@gmail.com>
2021-05-23 09:52:32 +0200

    Remove old files

--- macrosgenerator/macros.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('add').onclick = function(ev) {
-    var name = prompt("Enter a name for macro:");
-    var command = prompt("Enter the chat message/command, that should be executed:");
-    var keybind = prompt("Enter the key-code the macro should use:");
-
-    var output = document.getElementById('output').innerText;
-
-    document.getElementById('output').innerText = `${output}${name}:${command}:${keybind}\n`;
-}
